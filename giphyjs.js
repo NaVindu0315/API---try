7e37c64d435983396b33ca672c8a8dfc4e3aa108
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2023-11-20 22:36:55 +0530

    added neww jss

--- giphyjs.js
+++ giphyjs.js
@@ -1,1 +1,17 @@
-fetch("api.giphy.com/v1/gifs/random")
+
+fetch("https://api.giphy.com/v1/gifs/random?api_key=43EwelydlVY7dxERDiMUfAy9QfFEtMO4")
+.then(function(response)
+{
+    return response.json();
+}
+)
+.then(function(jsondata)
+{
+    console.log(jsondata);
+    var gifurl = jsondata.data.images.original.url;
+    console.log(gifurl);
+    var gifmg = document.createElement("img");
+    gifmg.setAttribute("src",gifurl);
+    document.body.appendChild(gifmg);
+}
+)
